6809b0cd3de4f20433ae771cf21b1bf1ff3eec60
OdedAlon <oded345alon@gmail.com>
2021-04-21 21:26:01 +0300

    almost ready

--- js/meme-service.js
+++ js/meme-service.js
@@ -2,9 +2,24 @@
 
 // var gKeywords = {'happy': 12,'funny puk': 1};
 var gImgs = [
-    {id: 1, url: 'img/1.jpg', keywords: ['happy']},
+    {id: 1, url: 'img/1.jpg', keywords: ['tramp']},
     {id: 2, url: 'img/2.jpg', keywords: ['happy', 'dog']},
-    {id: 3, url: 'img/3.jpg', keywords: ['happy', 'dog']},
+    {id: 3, url: 'img/3.jpg', keywords: ['happy', 'dog', 'baby']},
+    {id: 4, url: 'img/4.jpg', keywords: ['happy', 'cat', 'computer']},
+    {id: 5, url: 'img/5.jpg', keywords: ['happy', 'baby', 'child', 'yes']},
+    {id: 6, url: 'img/6.jpg', keywords: ['happy', 'amazing']},
+    {id: 7, url: 'img/7.jpg', keywords: ['surprise', 'baby']},
+    {id: 8, url: 'img/8.jpg', keywords: ['happy', 'satisfied']},
+    {id: 9, url: 'img/9.jpg', keywords: ['happy', 'baby', 'amused', 'prank', 'maggach']},
+    {id: 10, url: 'img/10.jpg', keywords: ['happy', 'laughs', 'obama']},
+    {id: 11, url: 'img/11.jpg', keywords: ['suffocating', 'strangle', 'wrestler', 'wrestlers', 'beatings']},
+    {id: 12, url: 'img/12.jpg', keywords: ['you', 'surprise']},
+    {id: 13, url: 'img/13.jpg', keywords: ['happy', 'toast', 'wine']},
+    {id: 14, url: 'img/14.jpg', keywords: ['scary', 'sunglasses', 'beatings', 'serious']},
+    {id: 15, url: 'img/15.jpg', keywords: ['exactly', 'satisfied', 'you']},
+    {id: 16, url: 'img/16.jpg', keywords: ['laughs', 'embarrassment', 'awkward', 'tears']},
+    {id: 17, url: 'img/17.jpg', keywords: ['victory', 'two']},
+    {id: 18, url: 'img/18.jpg', keywords: ['to cheer', 'toy', 'toys', 'encouraging']},
     ]; 
 var gMeme = { 
     selectedImgId: 1, 
